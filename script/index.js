15e02f6c48d3d454a8ff04e3c7fc8a5c38a932e4
Denselle0311 <144681965+Denselle0311@users.noreply.github.com>
2024-06-20 23:04:52 +0800

    add functionality to the buttons

--- script/index.js
+++ script/index.js
@@ -25,3 +25,47 @@ document.body.onmousedown = () => (mousedown = true);
 document.body.onmouseup = () => (mousedown = false);
 
 colorPicker.onchange = () => checkMode();
+
+buttons.forEach(item => { item.addEventListener('click', e => {
+        let targetId = e.target.getAttribute('id');
+        
+        switch(targetId) {
+            case 'color-mode': e.target.classList.add('active');
+                        randomMode.classList.remove('active');
+                        eraser.classList.remove('active');
+                        activeMode = COLOR_MODE;
+                        checkMode();
+            break
+            case 'rnd-color': e.target.classList.add('active');
+                        colorMode.classList.remove('active');
+                        eraser.classList.remove('active');
+                        activeMode = RANDOM_MODE;
+                        checkMode();
+            break
+            case 'grid-lines': e.target.classList.toggle('active');
+                            const grids = [...gridContainer.children];
+                            if(e.target.className == 'active') {
+                                for(item of grids) {
+                                    checkGridLines(item);
+                                }
+                                console.log('gridlines acive');
+                                return
+                            }
+                            for(item of grids) {
+                                item.style.border = 'none';
+                            }
+                            console.log('lines not active');
+            break
+            case 'eraser': e.target.classList.add('active');
+                        colorMode.classList.remove('active');
+                        randomMode.classList.remove('active');
+                        activeMode = ERASER_MODE;
+                        checkMode();
+            break
+            case 'clear': clearAll(ERASER_COLOR);
+            break
+        }
+    });
+})
+gridSlider.oninput = e => setupGrid(e.target.value);
+
